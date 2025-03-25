e9269dd4c3dd65d0a6556a304c9952bf5ac37fe3
Natalia <naty-aragao@hotmail.com>
2025-03-25 16:54:39 -0300

    este é o hello world

---
+++ src/main/java/io/github/natysls/produtossapi/ProdutosApiApplication.java
@@ -0,0 +1,11 @@
+package io.github.natysls.produtossapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class ProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/natysls/produtossapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.natysls.produtossapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
